b70230bc4ae3763542de85a1b8d50fd324f5eb49
IamdebashisD <adebashisdas626@gmail.com>
2025-05-26 15:24:02 +0530

    fixed the counter

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 function App() {
 
   let [counter, setCounter] = useState(0)
+  const [multipliedValue, setMultipliedValue] = useState(0)
 
   const addValue = () =>{
     if (counter < 20){
@@ -16,6 +17,14 @@ function App() {
     }
   }
 
+  const multiply = ()=>{
+    if (counter>=0){
+      setMultipliedValue(counter * 2)
+    }
+  }
+  console.log(setMultipliedValue);
+  
+
   return (
     <>
       <h1>Chai aur react</h1>
@@ -23,6 +32,11 @@ function App() {
       <button onClick={addValue}>Add value</button>
       <br />
       <button onClick={removeValue}>Remove value</button>
+      <br />
+      <button onClick={multiply}>Multiply by 2</button>
+      <h1>
+        multiply result: {multipliedValue >= 0 ?multipliedValue : "click 'multiply' frist"}
+      </h1>
 
     </>
   )
